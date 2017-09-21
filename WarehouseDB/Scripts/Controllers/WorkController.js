b86af3770233da756d01f8b1342943c7db8e58ad
Гранкина Ольга <granckina.olga2011@yandex.ua>
2017-09-21 17:33:43 +0300

    json date

--- WarehouseDB/Scripts/Controllers/WorkController.js
+++ WarehouseDB/Scripts/Controllers/WorkController.js
@@ -8,7 +8,10 @@
         name: 'Naomi',
         address: '1600 Amphitheatre'
     };
-    
+    function formatJSONDate(jsonDate) {
+        var newDate = dateFormat(jsonDate, "mm/dd/yyyy");
+        return newDate;
+    }
     $scope.userList = [];
   
         $http({
@@ -21,16 +24,25 @@
         }).
   then(function(response) {
       $scope.userList = response.data.rows;
+      //$scope.userList.value.forEach(function callback(currentValue, index, array) {
+      //    currentValue.Data_priyoma = new Date(parseInt(currentValue.Data_priyoma.substr(6)));
+      //    currentValue.Data_vydachi = new Date(parseInt(currentValue.Data_vydachi.substr(6)));
+      //});
       angular.forEach($scope.userList, function (obj) {
           obj["showEdit"] = true;
           obj["showSub"] = false;
+          obj.value.Data_priyoma = new Date(parseInt(obj.value.Data_priyoma.substr(6))).toLocaleDateString();
+          obj.value.Data_vydachi = new Date(parseInt(obj.value.Data_vydachi.substr(6))).toLocaleDateString();
+
       })
   });
         $scope.toggleEdit = function (emp) {
             emp.showEdit = emp.showEdit ? false : true;
         };
 
-     
+        $scope.cancelEdit = function (emp) {
+            emp.showEdit = emp.showEdit ? false : true;
+        };
     $scope.childInfo = function (emp) {
         emp.showSub = emp.showSub ? false : true;
         
